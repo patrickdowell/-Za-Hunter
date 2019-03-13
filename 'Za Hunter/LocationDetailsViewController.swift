b43862fc7559c0a04e79c00cba4104809475e8ae
Patrick Dowell <patrickdowell@patrick-ds-macbook-air.local>
2019-03-13 11:21:26 -0500

    Added segue to LocationDetailsController

---
+++ 'Za Hunter/LocationDetailsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LocationDetailsViewController.swift
+//  'Za Hunter
+//
+//  Created by Patrick Dowell on 3/12/19.
+//  Copyright © 2019 Patrick Dowell. All rights reserved.
+//
+
+import UIKit
+
+class LocationDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+}
